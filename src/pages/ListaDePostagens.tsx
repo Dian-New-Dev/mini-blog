d99c4744ea5833c2f230ec5f2fc53fadc81ae244
Diego <diannewdev@gmail.com>
2024-10-08 15:44:05 -0400

    titulos de postagens estao sendo renderizados via map

--- src/pages/ListaDePostagens.tsx
+++ src/pages/ListaDePostagens.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ListaDePostagens: React.FC = () => {
-    
-    const arrayDePosts = localStorage.getItem("arrayDeObjetosOG")
-    console.log(JSON.parse(arrayDePosts));
 
+    const [listaDePosts, setListaDePosts] = useState<string[]>([])
+
+    useEffect(() =>{
+        const arrayDePosts = localStorage.getItem("arrayDeObjetosOG")
+        if (arrayDePosts !== null) {
+            mapearPosts(arrayDePosts)
+        }
+
+        
+    }, [])
+
+    function mapearPosts(array) {
+        const parsedArray = JSON.parse(array);
+        console.log(parsedArray[0]);
+
+        const lista = parsedArray.map((item, index) => (
+            <div key={index} className={``}>
+                {item.titulo}
+            </div>
+        ));
+        setListaDePosts(lista)
+
+        }
+
+    
     return (
-        <h1>Aqui deve ser a ListaDePostagens</h1>
+        <div>
+            {listaDePosts}
+        </div>
+        
     );
 };
 
