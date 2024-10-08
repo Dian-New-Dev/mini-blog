fd2916334268e118b766a2d32c2a9a699bc3f73f
Diego <diannewdev@gmail.com>
2024-10-08 01:35:28 -0400

    preencher formulario de novo post insere novo post em array de objetos

--- src/pages/NovoPost.tsx
+++ src/pages/NovoPost.tsx
@@ -1,48 +1,64 @@
 import React, {useEffect, useState} from 'react';
-import { Link } from 'react-router-dom';
-import Post from '../data/ArmazemDePosts';
+
+interface arrayDeObjetos {
+    titulo: string;
+    corpo: string;
+}
 
 const NovoPost: React.FC = () => {
 
-    useEffect(() => {
-        const post1 = new Post('barbaridade', 'tche');
-        const post2 = new Post('lala', 'teetete')
-        post1.inserirnoArray();
-        post2.inserirnoArray();
-    }, [])
+    const [itemTitle, setItemTitle] = useState<string[]>([]);
+    const [itemBody, setItemBody] = useState<string[]>([]);
+    const [arrayDeObjetos, setArrayDeObjetos] = useState<arrayDeObjetos[]>([]);
     
+    function handleInputTitle(e:React.ChangeEvent<HTMLInputElement>) {
+        setItemTitle(e.target.value)
+    }
+
+    function handleInputBody(e:React.ChangeEvent<HTMLInputElement>) {
+        setItemBody(e.target.value)
+    }
     
     function handleSubmit(e: React.FormEvent) {
         e.preventDefault();
+
+        setArrayDeObjetos(prevArray => [...prevArray, {titulo: itemTitle, corpo: itemBody }]);
+
+            
+
+
     }
 
+    useEffect(() => {
+        console.log(arrayDeObjetos)
+    }, [arrayDeObjetos])
+
     return (
-        <p></p>
-        // <div 
-        // id="novo-post-container"
-        // className='
-        // grid
-        // place-items-center
-        // w-full
-        // h-full'>
-        //     <div>
-        //         <form className='
-        //         flex
-        //         flex-col
-        //         gap-4' 
-        //         onSubmit={handleSubmit}>
-        //             <label htmlFor="titulo">Título</label>
-        //             <input onChange={handleInputTitle} type="text" name='titulo' id="titulo"/>
+        <div 
+        id="novo-post-container"
+        className='
+        grid
+        place-items-center
+        w-full
+        h-full'>
+            <div>
+                <form className='
+                flex
+                flex-col
+                gap-4' 
+                onSubmit={handleSubmit}>
+                    <label htmlFor="titulo">Título</label>
+                    <input onChange={handleInputTitle} type="text" name='titulo' id="titulo"/>
                     
-        //             <label htmlFor="novo-post">Corpo do Texto</label>
-        //             <textarea onChange={handleInputBody} name="novo-post" id="novo-post" />
+                    <label htmlFor="novo-post">Corpo do Texto</label>
+                    <textarea onChange={handleInputBody} name="novo-post" id="novo-post" />
                     
                 
-        //             <button type='submit'>Postar</button>
-        //         </form>
-        //     </div>
+                    <button type='submit'>Postar</button>
+                </form>
+            </div>
             
-        // </div>
+        </div>
     );
 
 };
