ef65b4bae85493c0216f15d81dfeb347022c4ed8
Diego <diannewdev@gmail.com>
2024-10-09 11:34:21 -0400

    adicionei useNavigate no NovoPost para voltar a homepage quando clica em submeter

--- src/pages/NovoPost.tsx
+++ src/pages/NovoPost.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import { useOutletContext } from 'react-router-dom';
+import { useOutletContext, useNavigate } from 'react-router-dom';
 
 interface arrayDeObjetosOG { //para tipagem de array de objetos
     titulo: string;
@@ -7,6 +7,8 @@ interface arrayDeObjetosOG { //para tipagem de array de objetos
 }
 
 const NovoPost: React.FC = () => {
+
+    const navigate = useNavigate();
 
     const [reRenderizar, setReRenderizar] = useOutletContext();
 
@@ -35,6 +37,8 @@ const NovoPost: React.FC = () => {
     
         // Atualiza o estado que deve acionar a re-renderização
         setReRenderizar(prev => prev + 1);
+
+        navigate("/")
     }
     
 
@@ -122,88 +126,3 @@ const NovoPost: React.FC = () => {
 };
 
 export default NovoPost;
-
-// import React, {useEffect, useState} from 'react';
-// import { useOutletContext } from 'react-router-dom';
-
-// interface arrayDeObjetosOG { //para tipagem de array de objetos
-//     titulo: string;
-//     corpo: string;
-// }
-
-// const NovoPost: React.FC = () => {
-
-//     const [reRenderizar, setReRenderizar] = useOutletContext();
-
-//     function novaPostagem() {
-//         setReRenderizar(prev => prev + 1)
-//         console.log('')
-
-//     }
-
-//     const [itemTitle, setItemTitle] = useState<string[]>([]);
-//     const [itemBody, setItemBody] = useState<string[]>([]);
-//     const [arrayDeObjetosOG, setArrayDeObjetosOG] = useState<arrayDeObjetosOG[]>([]);
-
-//     function handleInputTitle(e:React.ChangeEvent<HTMLInputElement>) {
-//         setItemTitle(e.target.value)
-//     }
-
-//     function handleInputBody(e:React.ChangeEvent<HTMLInputElement>) {
-//         setItemBody(e.target.value)
-//     }
-    
-//     function handleSubmit(e: React.FormEvent) {
-//         e.preventDefault();
-//         const novoPost = {titulo: itemTitle, corpo: itemBody};
-//         setArrayDeObjetosOG(prevArray => [...prevArray, novoPost]);
-//     }
-
-//     useEffect(() => {
-//         // Carregar dados do localStorage ao montar o componente
-//         const savedPosts = localStorage.getItem('arrayDeObjetosOG');
-//         if (savedPosts) {
-//             setArrayDeObjetosOG(JSON.parse(savedPosts));
-//         }
-//     }, []);
-
-//     useEffect(() => {
-//         if (arrayDeObjetosOG.length > 0) { //sem esse if, o useEffect resetaria o localstorage a cada montagem
-//             console.log(arrayDeObjetosOG)
-//             localStorage.setItem("arrayDeObjetosOG", JSON.stringify(arrayDeObjetosOG));
-//         }
-//     }, [arrayDeObjetosOG])
-
-//     return (
-//         <div 
-//         id="novo-post-container"
-//         className='
-//         grid
-//         place-items-center
-//         w-full
-//         h-full'>
-//             <div>
-//                 <form className='
-//                 flex
-//                 flex-col
-//                 gap-4' 
-//                 onSubmit={handleSubmit}>
-//                     <label htmlFor="titulo">Título</label>
-//                     <input onChange={handleInputTitle} type="text" name='titulo' id="titulo"/>
-                    
-//                     <label htmlFor="novo-post">Corpo do Texto</label>
-//                     <textarea onChange={handleInputBody} name="novo-post" id="novo-post" />
-                    
-                
-//                     <button onClick={novaPostagem} type='submit'>Postar</button>
-//                 </form>
-//             </div>
-
-           
-            
-//         </div>
-//     );
-
-// };
-
-// export default NovoPost;
