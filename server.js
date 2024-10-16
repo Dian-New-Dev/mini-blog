a190dcef271165e7bf4f3bf9a71f12b4a6a61c41
Diego <diannewdev@gmail.com>
2024-10-16 09:06:24 -0400

    codigo basico do server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+// server.js: Configura e inicia o servidor Express para gerenciar 
+// requisições HTTP. Serve como ponto de entrada 
+// principal para o backend da aplicação.
+
+const express = require('express')
+const app = express()
+
+app.get("/api", (req, res) => {
+    req.json({"users" : ["userOne", "UserTwo", "userThree"] })
+})
+
+app.listen(5000, () => (console.log('server started on port 5000')))
