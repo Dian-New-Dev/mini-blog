93c5f6842cda0143b475829dc6f21334f07ea9e5
Diego <diannewdev@gmail.com>
2024-10-17 19:44:33 -0400

    componente GerenciarPosts lista os posts do usuario

--- client/src/pages/GerenciarPosts.tsx
+++ client/src/pages/GerenciarPosts.tsx
@@ -1,11 +1,50 @@
 import React from 'react';
+import { useListaDePostagens } from '../hooks/useListaDePostagens';
+import { useOutletContext } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
+
 
 const GerenciarPosts: React.FC = () => {
 
+    const [reRenderizar, setReRenderizar, clicouEmLinks, setClicouEmLinks] = useOutletContext();
+
+
+    const { listaDePosts, semPosts } = useListaDePostagens(reRenderizar);
+
+    console.log(listaDePosts)
     
 
     return (
-        <h1>Aqui deve ser a GerenciarPosts</h1>
+        <div>
+            <p>Seus posts</p>
+
+            <div className=''>
+                {
+                    listaDePosts.map((item, index) => (
+                        <div className='border bg-green-950 text-green-200 flex'>
+                            <div className='w-[80%]'>
+                                <div key={index} className='hover:underdivne hover:scale-105 hover:text-green-300'>
+                                    <NavLink to={`/post/${item.titulo}`}>
+                                        {item.titulo}
+                                    </NavLink>
+                                </div>
+                                <div>
+                                    {item.corpo}
+                                </div>
+                                
+                            </div>
+                            <div className='flex gap-4 w-[20%]'>
+                                <button>Apagar</button>
+                                <button>Editar</button>
+                            </div>
+                        
+                        </div>
+
+                    ))
+                }
+            </div>
+        </div>
     );
 };
 
